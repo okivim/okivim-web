1de5d57214da19be2aff77a42ac9b57f8e65c517
Mathiew82 <amateo1982@yahoo.es>
2026-01-31 12:12:52 +0100

    style: add navbar for screenshot wrapper

--- src/components/HomepageScreenshot/index.tsx
+++ src/components/HomepageScreenshot/index.tsx
@@ -4,8 +4,13 @@ import styles from "./styles.module.css";
 export default function HomepageScreenshot() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <div className={styles.screenshotWrapper}>
+    <section className={styles.screenshotWrapper}>
+      <div className={styles.screenshotWrapperNavbar}>
+        <span className={`${styles.cBtn} ${styles.redBtn}`}></span>
+        <span className={`${styles.cBtn} ${styles.yellowBtn}`}></span>
+        <span className={`${styles.cBtn} ${styles.greenBtn}`}></span>
+      </div>
       <img src="./img/screenshot.png" className={styles.screenshot} />
-    </div>
+    </section>
   );
 }
