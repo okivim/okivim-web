3f5b482479253b96ceca0e1939f11157c0fdf376
Mathiew82 <amateo1982@yahoo.es>
2026-01-31 09:31:48 +0100

    style: improve header homepage and primary color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,22 @@ function HomepageHeader() {
             className="button button--secondary button--lg"
             to="/docs/intro"
           >
-            Docusaurus Tutorial - 5min ⏱️
+            Docs
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-arrow-right-icon lucide-arrow-right"
+            >
+              <path d="M5 12h14" />
+              <path d="m12 5 7 7-7 7" />
+            </svg>
           </Link>
         </div>
       </div>
