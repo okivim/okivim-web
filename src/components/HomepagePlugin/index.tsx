53ddc5d5cf7229e45dbc4b16db5fdd9c6ec1232a
Mathiew82 <amateo1982@yahoo.es>
2026-02-08 21:19:10 +0100

    feat: create HomepagePlugin component to render slider content

--- src/components/HomepagePlugin/index.tsx
+++ src/components/HomepagePlugin/index.tsx
@@ -1,16 +1,16 @@
 import styles from "./styles.module.css";
 
-export default function HomepagePlugin({ title, description, image, reverse }) {
+export default function HomepagePlugin({ title, description, image }) {
   return (
     <section className={styles.pluginWrapper}>
-      <div
-        className={`${styles.pluginContent} ${reverse ? styles.reverse : ""}`}
-      >
-        <div className={styles.pluginInfo}>
-          <h2>{title}</h2>
-          <div>{description}</div>
+      <div className={styles.pluginContent}>
+        <div className={styles.pluginItem}>
+          <div className={styles.pluginInfo}>
+            <h3>{title}</h3>
+            <div>{description}</div>
+          </div>
+          <img src={image} className={styles.screenshot} />
         </div>
-        <img src={image} className={styles.screenshot} />
       </div>
     </section>
   );
